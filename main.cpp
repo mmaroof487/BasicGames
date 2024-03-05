e57764021ea075c215bd1fa93b4beacafc52c4ba
Mohammad Maroof <mmaroof487@gmail.com>
2024-03-05 18:30:25 +0530

    feat: added tictactoe game

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 void rps();
 void ht();
 void ng();
+void tt();
 
 int main()
 {
@@ -20,6 +21,7 @@ int main()
        << "1. Rock-Paper-Scissor" << endl
        << "2. Head-Tails" << endl
        << "3. Number Guessing Game" << endl
+       << "4. Tic-Tac-Toe Game" << endl
        << endl
        << "Enter your Choice: ";
   cin >> choice;
@@ -35,6 +37,9 @@ int main()
   case 3:
     ng();
     break;
+  case 4:
+    tt();
+    break;
   default:
     cout << "Invalid Input!";
   }
@@ -44,6 +49,8 @@ int main()
   cin >> exit;
   main();
 }
+
+// ROCK-PAPER-SCISSOR
 
 char inputUser();
 int getComputerChoice(string option[]);
@@ -148,6 +155,8 @@ void result(int comp, char user)
   }
 }
 
+// HEAD-TAIL
+
 void ht()
 {
   string guess, heta[] = {"head", "tail"};
@@ -179,6 +188,8 @@ void ht()
   }
 }
 
+// NUMBER-GUESSING
+
 void ng()
 {
   int guess, num, tries = 0;
@@ -209,4 +220,94 @@ retry:
   }
   cout << endl
        << "YES! That is right. It took you " << tries << " tries" << endl;
+}
+
+// TIC-TAC-TOE
+
+void drawBoard(char board[3][3]);
+bool checkWin(char board[3][3], char player);
+
+void tt()
+{
+
+  char board[3][3] = {{' ', ' ', ' '},
+                      {' ', ' ', ' '},
+                      {' ', ' ', ' '}};
+  char player = 'X';
+  int row, col;
+  int turn;
+
+  cout << endl
+       << "Welcome to Tic-Tac-Toe!\n";
+
+  for (turn = 0; turn < 9; turn++)
+  {
+    drawBoard(board);
+
+    while (true)
+    {
+      cout << "Player " << player
+           << ", enter row (0-2) and column (0-2): ";
+      cin >> row >> col;
+
+      if (board[row][col] != ' ' || row < 0 || row > 2 || col < 0 || col > 2)
+      {
+        cout << "Invalid move. Try again.\n";
+      }
+      else
+      {
+        break;
+      }
+    }
+
+    board[row][col] = player;
+
+    drawBoard(board);
+
+    if (checkWin(board, player))
+    {
+      drawBoard(board);
+      cout << endl
+           << "Player " << player << " wins!" << endl;
+      break;
+    }
+
+    player = (player == 'X') ? 'O' : 'X';
+  }
+
+  if (turn == 9 && !checkWin(board, 'X') && !checkWin(board, 'O'))
+  {
+    cout << endl
+         << "It's a draw!" << endl;
+  }
+}
+
+void drawBoard(char board[3][3])
+{
+  cout << "-------------\n";
+  for (int i = 0; i < 3; i++)
+  {
+    cout << "| ";
+    for (int j = 0; j < 3; j++)
+    {
+      cout << board[i][j] << " | ";
+    }
+    cout << "\n-------------\n";
+  }
+}
+
+bool checkWin(char board[3][3], char player)
+{
+  for (int i = 0; i < 3; i++)
+  {
+    if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+      return true;
+    if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
+      return true;
+  }
+  if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+    return true;
+  if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+    return true;
+  return false;
 }
